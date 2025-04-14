6b9462acfb58e2fe7d9e9e1e16fcf608daf724e9
jart82 <agarin3@gmail.com>
2025-04-14 13:22:59 +0100

    dynamically showed names clicked on task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,8 +15,13 @@ import { TaskComponent } from './task/task.component';
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 
   onSelectUser(id: string) {
-    console.log('Selected user with id ' + id);
+    this.selectedUserId = id;
   }
 }
